06d6f6a488ac34809c592ec7b83975ed442f8f26
Antonio Thomacelli <tonnytg@gmail.com>
2021-12-13 22:43:23 -0300

    feat: update orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { OrderStatus } from './entities/order.entity';
+import { TokenGuard } from './../accounts/token.guard';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards,} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { MessagePattern, Payload, Transport } from '@nestjs/microservices';
+import { KafkaMessage } from '@nestjs/microservices/external/kafka.interface';
 
+@UseGuards(TokenGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
@@ -19,7 +24,7 @@ export class OrdersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(id);
+    return this.ordersService.findOneUsingAccount(id);
   }
 
   @Patch(':id')
@@ -27,8 +32,18 @@ export class OrdersController {
     return this.ordersService.update(id, updateOrderDto);
   }
 
+  // status code correto para remover
+  @HttpCode(204)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.ordersService.remove(id);
+  }
+
+  @MessagePattern('transactions_result')
+  async consumerUpdateStatus(@Payload() message: KafkaMessage) {
+    const data = message.value as any;
+    console.log(data);
+    const { id, status } = data as { id: string; status: OrderStatus };
+    await this.ordersService.update(id, { status });
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,35 +1,76 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
+import { AccountStorageService } from './../accounts/account-storage/account-storage.service';
+import { Order } from './entities/order.entity';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { Order } from './entities/order.entity';
+import { InjectModel } from '@nestjs/sequelize';
+import { EmptyResultError } from 'sequelize';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectModel(Order)
     private orderModel: typeof Order,
+    private accountStorage: AccountStorageService,
+    @Inject('KAFKA_PRODUCER')
+    private kafkaProducer: Producer,
   ) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    return this.orderModel.create(createOrderDto);
+  async create(createOrderDto: CreateOrderDto) {
+    const order = await this.orderModel.create({
+      ...createOrderDto,
+      account_id: this.accountStorage.account.id,
+    });
+    this.kafkaProducer.send({
+      topic: 'transactions',
+      messages: [
+        {
+          key: 'transactions',
+          value: JSON.stringify({
+            ...createOrderDto,
+            ...order.toJSON(),
+          }),
+        },
+      ],
+    });
+    return order;
   }
 
   findAll() {
-    return this.orderModel.findAll();
+    return this.orderModel.findAll({
+      where: {
+        account_id: this.accountStorage.account.id,
+      },
+      order: [['created_at', 'DESC']],
+    });
+  }
+
+  findOneUsingAccount(id: string) {
+    return this.orderModel.findOne({
+      where: {
+        id,
+        account_id: this.accountStorage.account.id,
+      },
+      rejectOnEmpty: new EmptyResultError(`Order with ID ${id} not found`),
+    });
   }
 
   findOne(id: string) {
     return this.orderModel.findByPk(id);
   }
 
+  // promessa para atualizar 1 id
   async update(id: string, updateOrderDto: UpdateOrderDto) {
-    const order = await this.findOne(id);
+    const account = this.accountStorage.account;
+    const order = await (account
+      ? this.findOneUsingAccount(id)
+      : this.findOne(id));
     return order.update(updateOrderDto);
   }
-
+  // promessa para remover 1 id
   async remove(id: string) {
-    const order = await this.findOne(id);
+    const order = await this.findOneUsingAccount(id);
     return order.destroy();
   }
 }
